f332db2bc6ee042546c9cc1d094930fb38b7164e
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2023-10-22 20:44:01 +0530

    update

---
+++ 2-values.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("go"+"lang")
+	// go+lang
+	fmt.Println("1+1 =", 1+1)
+	// 1+1 = 2
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// 7.0/3.0 = 2.3333333333333335
+	fmt.Println("7/3 =", 7/3)
+	fmt.Println(true && false)
+	// false
+	fmt.Println(true || false)
+	// true
+	fmt.Println(!true)
+	// false
+}
